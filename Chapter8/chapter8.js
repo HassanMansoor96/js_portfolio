e2dd56ac1f8fcc1ef34384dab8acf55c7f6056d5
Hassan <98019174+HassanMansoor96@users.noreply.github.com>
2022-01-21 15:35:21 +0200

    Add files via upload

---
+++ Chapter8/chapter8.js
@@ -0,0 +1,8 @@
+var carDetails = {Brand:"Infinity", Model:"Q60", Year:"2021", Condition:"Good!"};
+
+
+
+document.getElementById("brand").innerHTML = carDetails.Brand;
+document.getElementById("model").innerHTML = carDetails.Model;
+document.getElementById("year").innerHTML = carDetails.Year;
+document.getElementById("condition").innerHTML = carDetails.Condition;
